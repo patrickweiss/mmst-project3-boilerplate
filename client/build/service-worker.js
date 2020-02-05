53154c1f627ebcf9104faeac1c7b1ac2c9ad59b5
pyrrhos-msits <56028011+pyrrhos-msits@users.noreply.github.com>
2020-02-05 09:01:36 +0100

    Nach dem Build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a57bcaf7b88dc2db2e3b09e3c1743a4e.js"
+  "/precache-manifest.ffba7f26e31612f01a7c0db8c6538344.js"
 );
 
 self.addEventListener('message', (event) => {
