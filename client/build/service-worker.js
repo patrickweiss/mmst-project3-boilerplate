2b35576b699d9fe04c38a39d2bc64754ad38a7d6
pyrrhos-msits <56028011+pyrrhos-msits@users.noreply.github.com>
2020-02-10 11:00:50 +0100

    Icon

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4506bb615654d302680df79089febb66.js"
+  "/precache-manifest.415cf80af3a5f3ef2026c8a51e8c0e32.js"
 );
 
 self.addEventListener('message', (event) => {
